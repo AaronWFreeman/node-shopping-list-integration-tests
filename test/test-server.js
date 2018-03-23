edd269dfd65ca194d208297d0be69fe795a5d897
Aaron Freeman <Freeman0483@gmail.com>
2018-03-23 14:27:41 -0700

    node-shopping-list-tests

--- test/test-server.js
+++ test/test-server.js
@@ -61,7 +61,6 @@ describe('Shopping List', function() {
         });
       });
   });
-
   // test strategy:
   //  1. make a POST request with data for a new item
   //  2. inspect response object and prove it has right
@@ -136,6 +135,73 @@ describe('Shopping List', function() {
       .then(function(res) {
         return chai.request(app)
           .delete(`/shopping-list/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
+});
+
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  it('should list recipe items on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+  it('should add a recipe item on POST', function () {
+    const newRecipe = {name: 'pizza', ingredients: ['sauce', 'dough', 'pepperoni']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+      });
+  });
+  it('should update a recipe item on PUT', function() {
+    const updateData = {
+      name: 'foo',
+      ingredients: ['butts', 'bats']
+    };
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
+  it('should delete a recipe item on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
       })
       .then(function(res) {
         expect(res).to.have.status(204);
